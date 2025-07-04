6691ee0cbcb8b0fb4a193a626dd25f410193df87
Manish Yadav <manishyadav14127@gmail.com>
2025-07-04 22:16:17 +0530

    backend cors issue

--- backend/lib/utils.ts
+++ backend/lib/utils.ts
@@ -10,8 +10,8 @@ export const generateToken = (userId:string,res:any) => {
     res.cookie("jwt",token,{
         maxAge: 2*24*60*60*1000,
         httpOnly:true,
-        sameSite:"strict",
-        secure: process.env.NODE_ENV !== "development"
+        sameSite:"none",
+        secure: false
     })
 
     return token;
